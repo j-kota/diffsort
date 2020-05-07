8fc2a685bab3bfd4a213f16c68f1e6c8f622b1cb
j-kota <57915467+j-kota@users.noreply.github.com>
2020-05-06 21:34:09 -0400

    Add files via upload

---
+++ dnnRanks.py
@@ -0,0 +1,174 @@
+import numpy as np
+from random import randint
+import collections
+import pickle
+from torch.utils import data
+import matplotlib.pyplot as pp
+from pylab import *   
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import math
+from functools import reduce
+from softRank import softRank
+from MyReLU import MyReLU
+
+
+def mayberand(a,b,TF):
+    if TF==True:
+        return randint(a,b)
+    else:
+        return b
+    
+
+n_cols = 5
+n_rows = 10000  #need 10,000
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Hyper-parameters 
+input_size = n_cols      # n_jobs * n_tasksperjob * 2    (machine ID, duration)
+hidden_size = n_cols     # math.ceil( 2.0/3.0 * input_size )
+num_classes = 10
+num_epochs = 100
+batch_size = 100
+learning_rate = 0.001
+
+def ssum(l):
+    c = 0
+    for i in range(0,len(l)):
+        c = c+l[i]
+    return c
+
+def ranks(l):
+    l = -1.0*np.array(l)
+    sigma = np.argsort(l)
+    sigma_inv = np.argsort(sigma)+1
+    return sigma_inv.tolist()
+
+#[4 ,1 ,6 ,3 ,2 , 0, 10,5 ,7 ,8 , 9]
+#this = [  55,999,23,344,567,1001, 1,43,22,9,4 ]
+#print("x = ", this)
+#print("ranks = ",  ranks(this) )
+#input("waiting")
+
+ranklabel = ranks( np.random.rand(n_cols) )
+x = [ sorted( np.random.rand(n_cols) ) for i in range(0,n_rows) ]
+y = [ ranklabel for i in x ]  # same label for every sample
+print("rank label = ", ranklabel)
+
+split_ind = math.ceil( 0.8*n_rows )
+
+# create training data
+training_x = x[:split_ind]    #[  x[i] for i in range(0,split_ind)  ]
+training_y = y[:split_ind]
+
+
+# convert to tensor
+training_x = torch.Tensor(training_x)
+training_y = torch.Tensor(training_y)
+
+
+# create testing data
+testing_x  = x[split_ind:]
+testing_y  = y[split_ind:]
+
+# convert to tensor
+testing_x = torch.Tensor(testing_x)
+testing_y = torch.Tensor(testing_y)
+
+
+train_tensor = data.TensorDataset( training_x, training_y )
+test_tensor  = data.TensorDataset(  testing_x,  testing_y )
+
+train_loader = data.DataLoader(dataset = train_tensor, batch_size = batch_size, shuffle = True)
+test_loader  = data.DataLoader(dataset = test_tensor,  batch_size = batch_size, shuffle = False)
+
+
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1  = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        #self.fc2 = nn.Linear(hidden_size, 1)
+        self.fc2  = nn.Linear(hidden_size, hidden_size)
+        self.srank  = softRank()
+        self.myrelu = MyReLU()
+    
+    def forward(self, x):
+        out = self.fc1(x)
+        #out = self.myrelu.apply(out)
+        #out = self.relu(out)
+        #out = self.fc2(out)
+        out = self.srank.apply(out)
+        
+        return out
+
+    
+model = NeuralNet(input_size, input_size, num_classes).to(device)   #JK the model lives on the device
+
+# Loss and optimizer
+#criterion = nn.CrossEntropyLoss()     #JK-when ready:
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+
+# Train the model
+loss_list =[] #JK
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):  
+        # Move tensors to the configured device
+        images = images.reshape(-1, n_cols).to(device)     # JK -1 means figure out #rows 
+        labels = labels.to(device)
+        
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels) 
+        
+        # Backward and optimize
+        optimizer.zero_grad()  #JK reset the gradient, the optimizer accumulates them if not reset (usage is for RNN apparently)
+        loss.backward()
+        #input("waiting")
+        optimizer.step()
+        loss_list.append(loss.item()) #JK
+        if (i+1) % 10 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+
+         
+# Test the model 
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+
+        outputs = model(images)     # forward function is applied to every sample in the minibatch
+        total += labels.size(0)
+        correct += torch.all( (outputs == labels).bool(),1 ).sum().item()   #JK this shows that the loaders output minibatches.
+
+        print("outputs:")
+        print(outputs)
+        print("labels:")
+        print(labels)
+        print("outputs == labels")
+        print( torch.all( (outputs == labels).bool(),1 ) )
+
+    print('Accuracy of the network on the test data: {} %'.format(100 * correct / total))
+
+# Save the model checkpoint
+#torch.save(model.state_dict(), 'softRankDNN_state.ckpt')  #99.7%
+torch.save(model.state_dict(), 'zeroDNN_state.ckpt')
+
+
+print("printing one output: \n")
+print(model( torch.Tensor([221,222,223,224,225])[None,:] ))
+
+
+
---
+++ softRank.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn.isotonic import IsotonicRegression
+from scipy.linalg import block_diag
+
+
+class softRank(torch.autograd.Function):
+    """
+    We can implement our own custom autograd Functions by subclassing
+    torch.autograd.Function and implementing the forward and backward passes
+    which operate on Tensors.
+    """
+
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+
+        # the input is a batch of tensors (theta) to rank
+        thetas = input.detach().numpy()
+
+        # get a list of forward (rankings) and backward (jacobian) values per sample
+        results_list = [ fwdRank(theta) for theta in thetas ]
+
+        # separate the fwd and bkwd results
+        ranks_list   = [ torch.Tensor( a ) for (a,b) in results_list ]
+        jacobi_list  = [ torch.Tensor( b ) for (a,b) in results_list ]
+
+        # the results are stacked to conform to batch processing format
+        ctx.save_for_backward(  torch.stack(jacobi_list)  )
+        return torch.stack( ranks_list )
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradient of the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        jacobians, = ctx.saved_tensors
+        N = len(grad_output)
+
+        M = len( grad_output[0] )
+        
+        # multiply each gradient by the jacobian for the corresponding sample
+        # then restack the results to preserve the batch gradients' format
+        grad_input = torch.stack( [ torch.matmul( grad_output[i] , jacobians[i] ) for i in range(0,N) ] )
+        
+        #print("Inside softRank backward")
+        #print("jacobians from saved: ", jacobians)
+        #print("grad_output = ", grad_output)
+        #print("about to return grad_input:", grad_input)
+        
+        return grad_input
+
+
+
+def fwdRank(theta):
+
+    # Define variables, constants and permutations
+    rho = np.arange(len(theta))[::-1] + 1
+    eps = (1.0 / 10.0)
+    x = np.arange(len(theta))[::-1]
+    z = -theta/eps
+    w = rho
+    sigma = np.argsort(theta)
+    sigma_inv = np.argsort(sigma)
+    s = z[sigma]
+
+    ir = IsotonicRegression()
+
+    # v is the solution to isotonic regression
+    v = ir.fit_transform(x,  s-w)
+
+    # This is the result of the ranking operator
+    answer = z - v[sigma_inv]
+
+    # blocks will hold sequential integers constant over
+    # each block of the isotonic regression
+    c = 0
+    blocks = [0]*len(v)
+    for i in range(1,len(v)):
+        if v[i] == v[i-1]:
+            blocks[i] = blocks[i-1]
+        else:
+            c += 1
+            blocks[i] = c
+
+    # compute |B_j|, the length of each block
+    blocklens = [1]
+    for i in range(1,len(blocks)):
+        if blocks[i] == blocks[i-1]:
+            blocklens[-1] +=1
+        else:
+            blocklens.append(1)
+
+    # compute the nonzero jacobian values
+    num_blocks = len(blocklens)
+    blockvals = 1.0/np.array(blocklens)
+
+    # create the block matrices that will occupy
+    # the (block) diagonal of the jacobian of v
+    jacobi_v_blox = [ np.ones( (blocklens[i],blocklens[i]) )*blockvals[i] for i in range(0,num_blocks) ]
+    jacobi_len = sum( blocklens )
+
+    # assemble v's jacobian operator from blocks
+    jacobi_v = block_diag( *jacobi_v_blox )
+
+    # jacobian operator for P wrt variable z = -theta/eps
+    I = np.identity( jacobi_v.shape[0] )
+    jacobi_P = ( (I-jacobi_v)[sigma_inv] )[:,sigma_inv]  
+    #jacobi_P = I-( (jacobi_v)[sigma_inv] )[:,sigma_inv]  
+    
+    # jacobian operator for P wrt original input theta (chain rule)
+    jacobi_P_theta = jacobi_P * (-1.0/eps)
+
+    # Notes:
+    # about deriv wrt w (rho): dz/d(s:w) = dz/ds : dz/dw = dz/ds : 0 do we want that?
+    # check Lemma 2 with operator interpretation of the jacobian (make sure it's not transposed)
+
+    return  answer ,  jacobi_P_theta 
+
+
+
+
+    
+if __name__ == "__main__":
+
+    torch.set_printoptions(precision=10)
+    sr = softRank()
+    
+
+    theta = torch.Tensor( [1.0,  5.0, 1000.0,  55.0, 3.0] )
+    theta.requires_grad = True
+    print("theta.requires_grad: ", theta.requires_grad)
+    
+    ranks =  sr.apply(theta)
+    
+    ranksum = torch.sum(ranks)
+    b = ranksum.backward()
+
+    print("ranks = ", ranks)
+    print("ranks.grad = ", ranks.grad)
+    print("ranksum.grad = ", ranksum.grad)
+    print("return value of backward():", b)
+    
+    
+
+    
